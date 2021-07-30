1ced0a7aecd11e0b72a34168169fe3b29dbc22df
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-07-30 18:55:40 +0700

    Update admins.py

--- GeezProject/modules/admins.py
+++ GeezProject/modules/admins.py
@@ -1,6 +1,6 @@
 # Calls Music 1 - Telegram bot for streaming audio in group calls
 # Copyright (C) 2021  VckyouuBitch
-# Recode By mondar111 From Mon-MusicProject
+# Recode By monkar111 From Mon-MusicProject
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
